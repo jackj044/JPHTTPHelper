8a35384ae5777743342507a4181b45bae4c7d2cf
Jatin Patel <jatinj044@gmail.com>
2023-10-05 18:50:11 -0400

    Internal to public

--- Sources/JPHTTPHelper/JPNetworkError.swift
+++ Sources/JPHTTPHelper/JPNetworkError.swift
@@ -10,11 +10,11 @@ import Foundation
 
 public struct JPNetworkError : Error
 {
-    let reason: String?
-    let httpStatusCode: Int?
-    let requestUrl: URL?
-    let requestBody: String?
-    let serverResponse: String?
+    public let reason: String?
+    public let httpStatusCode: Int?
+    public let requestUrl: URL?
+    public let requestBody: String?
+    public let serverResponse: String?
 
     public init(withServerResponse response: Data? = nil, forRequestUrl url: URL, withHttpBody body: Data? = nil, errorMessage message: String, forStatusCode statusCode: Int)
     {
--- Sources/JPHTTPHelper/JPRequestHandler.swift
+++ Sources/JPHTTPHelper/JPRequestHandler.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-protocol JPRequestHandlerProtocol {
+public protocol JPRequestHandlerProtocol {
     var url:URL {get set}
     var httpMethod :JPHttpMethods {get set}
 }
 
 public struct JPRequest:JPRequestHandlerProtocol {
-    var url: URL
-    var httpMethod: JPHttpMethods
-    var requestBody: Data?
+    public var url: URL
+    public var httpMethod: JPHttpMethods
+    public var requestBody: Data?
     
-    init(url: URL, httpMethod: JPHttpMethods, requestBody: Data? = nil) {
+    public init(url: URL, httpMethod: JPHttpMethods, requestBody: Data? = nil) {
         self.url = url
         self.httpMethod = httpMethod
         self.requestBody = requestBody ?? nil
